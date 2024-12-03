40f33a3e64601700f8c0038f0183721f2a6b902f
hberng00 <hberng00@estudiantes.unileon.es>
2024-12-03 18:11:31 +0100

    Renaming again

--- test_app.py
+++ test_app.py
@@ -6,13 +6,13 @@ import pytest
 from app import app
 
 @pytest.fixture
-def flask_test_client():
+def app_test_client():
     """Crea un cliente de prueba para la aplicación Flask."""
     with app.test_client() as client:
         yield client
 
-def test_home(flask_test_client):
+def test_home(app_test_client):
     """Prueba que la ruta principal devuelve el mensaje esperado."""
-    response = flask_test_client.get("/")
+    response = app_test_client.get("/")
     assert response.status_code == 200  # Verifica que el código de estado sea 200
     assert response.data == b"Hello, CI/CD with Docker!"  # Verifica el contenido

89ee59101bf88910e09aac7480688374dfd8888c
hberng00 <hberng00@estudiantes.unileon.es>
2025-01-08 09:30:15 +0100

    arreglo tests

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ import pytest
 from app import app
 
 @pytest.fixture(name="test_client")
-def firts_test_client():
+def test_client():
     """
     Fixture que proporciona un cliente de prueba para la aplicación Flask.
     Este cliente simula peticiones HTTP para realizar las pruebas.
@@ -39,32 +39,6 @@ def test_greet(test_client):
     response = test_client.get(f'/greet/{name}')
     assert f'Hello, {name}!'.encode() in response.data
 
-def test_add_valid(test_client):
-    """
-    Test para la ruta "/add" con datos válidos.
-    Verifica que la operación de adición devuelva el resultado correcto.
-    """
-    response = test_client.post('/add', json={'a': 10, 'b': 20})
-    data = response.get_json()
-    assert data['result'] == 30
-
-def test_add_missing_data(test_client):
-    """
-    Test para la ruta "/add" con datos faltantes.
-    Verifica que se maneje correctamente el error cuando faltan parámetros.
-    """
-    response = test_client.post('/add', json={'a': 10})
-    data = response.get_json()
-    assert 'error' in data
-
-def test_add_invalid_data(test_client):
-    """
-    Test para la ruta "/add" con datos inválidos.
-    Verifica que se maneje correctamente el error cuando los datos no son válidos.
-    """
-    response = test_client.post('/add', json={'a': 'ten', 'b': 'twenty'})
-    data = response.get_json()
-    assert 'error' in data
 
 def test_404_error(test_client):
     """
